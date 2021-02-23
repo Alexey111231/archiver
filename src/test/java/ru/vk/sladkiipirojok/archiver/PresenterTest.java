7d1b3143d8a63cee34d6e3f8386caf0761457c29
Alexey <atestat9@mail.ru>
2021-02-24 02:38:39 +0300

    test, no archiver tests

--- src/test/java/ru/vk/sladkiipirojok/archiver/PresenterTest.java
+++ src/test/java/ru/vk/sladkiipirojok/archiver/PresenterTest.java
@@ -1,6 +1,56 @@
 package ru.vk.sladkiipirojok.archiver;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.vk.sladkiipirojok.archiver.domain.Archiver;
+import ru.vk.sladkiipirojok.archiver.input.Input;
+import ru.vk.sladkiipirojok.archiver.output.Output;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import static org.mockito.ArgumentMatchers.any;
+
 class PresenterTest {
+    private static Input input;
+    private static Output output;
+    private static Archiver archiver;
+
+    private static Presenter presenter;
+
+    @BeforeEach
+    void setUp() {
+        input = Mockito.mock(Input.class);
+        output = Mockito.mock(Output.class);
+        archiver = Mockito.mock(Archiver.class);
+
+        ArrayList<File> value = new ArrayList<>();
+        value.add(new File("test"));
+
+        Mockito.when(input.read()).thenReturn(value);
+        Mockito.when(archiver.archive(any(), any())).thenReturn(new ArrayList<>());
+        presenter = new Presenter(archiver, input, output);
+    }
+
+    @Test
+    void Archive_TestCallingClasses_CurrectNumberOfUnits() {
+        presenter.archive();
+
+        Mockito.verify(input, Mockito.times(1)).read();
+        Mockito.verify(output, Mockito.times(1)).write(any());
+        Mockito.verify(archiver, Mockito.times(1)).archive(any(), any());
+        Mockito.verify(archiver, Mockito.times(0)).unpack(any(), any());
+    }
+
+    @Test
+    void Unpack_TestCallingClasses_CurrectNumberOfUnits() {
+        presenter.unpack();
+
+        Mockito.verify(input, Mockito.times(1)).read();
+        Mockito.verify(output, Mockito.times(0)).write(any());
+        Mockito.verify(archiver, Mockito.times(0)).archive(any(), any());
+        Mockito.verify(archiver, Mockito.times(1)).unpack(any(), any());
+    }
 
 }
--- src/test/java/ru/vk/sladkiipirojok/archiver/domain/ZipArchiverTest.java
+++ src/test/java/ru/vk/sladkiipirojok/archiver/domain/ZipArchiverTest.java
@@ -1,7 +1,51 @@
 package ru.vk.sladkiipirojok.archiver.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
 
 class ZipArchiverTest {
+    @TempDir
+    File tempDir;
 
+    @Test
+    void Archive_AllCorrectFiles_AllFilesSave() {
+    }
+
+    @Test
+    void Archive_NotAllCorrectFiles_SkipNotCorrectFiles() {
+    }
+
+    @Test
+    void Archive_DuplicateName_ThrownIllegalStateException() {
+    }
+
+    @Test
+    void Archive_EmptyName_ThrownIllegalStateException() {
+    }
+
+    @Test
+    void Archive_FilesAndEmptyDir_SkipEmptyDir() {
+    }
+
+    @Test
+    void Archive_EmptyFilesList_Nothing() {
+    }
+
+    @Test
+    void Unpack_CorrectInput_AllFilesUnpack() {
+    }
+
+    @Test
+    void Unpack_ErrorFilesInput_SkipErrorFiles() {
+    }
+
+    @Test
+    void Unpack_ErrorArchiveName_ThrownIllegalStateException() {
+    }
+
+    @Test
+    void Unpack_EmptyFilesList_Nothing() {
+    }
 }
